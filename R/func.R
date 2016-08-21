a3e4f89379f5939a4b1535daf10701b7e1da60f7
jmcimula <jmkacim@gmail.com>
2016-08-21 13:18:15 +0200

    some other functions

--- R/func.R
+++ R/func.R
@@ -1,3 +1,43 @@
+
+#' Segmenting African Market From African Development Bank Group
+#'
+#' @details
+#'
+#' Status of the projects are approved, ongoing, lending and pipeline
+#'
+#' @export
+#'
+getAmount <- function (xx, yy){
+
+  w <- xml2::read_html(xx)
+
+  if ( stringr::str_trim(yy) == "approved" || stringr::str_trim(yy) == "ongoing"){
+
+    z <- rvest::html_nodes(w,"table")
+
+    z <- rvest::html_table(z)[[1]]
+
+    colnames(z) <- c("A","B")
+
+    amount <- dplyr::filter(z,stringr::str_trim(tolower(A)) %in% "total")
+
+    amount <- stringr::str_replace_all(amount[2], "[:alpha:]", "")
+
+  }else{
+
+    z <- rvest::html_nodes (w,"table tbody")
+
+    z <- rvest::html_text(z)
+
+    amount <- stringr::str_replace_all(z, "[:alpha:]", "")
+
+    amount <- stringr::str_trim (amount)
+  }
+
+  return (stringr::str_trim(amount))
+}
+
+
 #' Segmenting African Market From African Development Bank Group
 #'
 #' @details
@@ -226,3 +266,5 @@ getData <- function (x,link){
     }
         return (valr)
 }
+
+
