6bed5d439600a57314a7d9eccfa12e5c592c440b
jmcimula <jmkacim@gmail.com>
2016-08-22 07:25:50 +0200

    creating data.frame from input

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,17 @@
 #' Segmenting African Market From African Development Bank Group
 #'
+#' @param sector the name of the sector or list of sectors
+#' @param status you can filter the result of sectors by status.
+#'
+#' @details
+#'
+#' Reference for to visualize all statuses \code{\link{afr_project_st}()}
+#'
+#' Reference for to visualize all sectors \code{\link{afr_list_sector}()}
+#'
 #' @export
 #'
-afr_sector_df <- function (sector){
+afr_sector_df <- function (sector,status,na.rm = TRUE, ...){
 
   x <- sector
 
@@ -42,10 +51,18 @@ afr_sector_df <- function (sector){
 
             y <- xml2::read_html(link)
 
-            np <- getPageNumber(substr(y,unlist(gregexpr(pattern=st,y))[1],unlist(gregexpr(pattern=st,y))[1] + 30))
+            np <- getPageNumber(substr(y,unlist(gregexpr(pattern=st,y))[1],unlist(gregexpr(pattern=st,y))[1]+30))
 
-            dfr <- rbind(dfr, cbind(getData(np,link), sector = x[i] ) )
+            y <- getData(np,link)
+
+            dfr <- rbind(dfr, cbind(y, sector = x[i]))
          }
-    }
+  }
+
+##if(length(status) <> 0){
+##In progress
+##  dfr <- dplyr::filter(dfr,dfr$status %in% status)
+##}
+
   return(dfr)
 }
