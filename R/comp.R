35f56c4056c0efb768a5577e6851e9f031843adf
jmcimula <jmkacim@gmail.com>
2016-10-17 13:16:49 +0200

    Comparison of project

--- R/comp.R
+++ R/comp.R
@@ -1,6 +1,6 @@
 #' Project comparison
 #'
-afr_data_comp(project, src.comp = c("un", "worldbank", "eu")){
+afr_data_comp <- function(project, src.comp = c("un", "worldbank", "eu")){
 
   src <- match.arg(src.comp)
   site <- base::match(scr, afr_extract())
