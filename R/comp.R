39586e18e76c3bd4f9d77a02f18456b931564074
jmcimula <jmkacim@gmail.com>
2016-10-17 13:49:26 +0200

    Comparison of project

--- R/comp.R
+++ R/comp.R
@@ -1,8 +1,17 @@
 #' Project comparison
 #'
-afr_data_comp <- function(project, src.comp = c("un", "worldbank", "eu")){
+afr_data_comp <- function(project, src.comp=c("un", "worldbank", "eu"), search=TRUE){
 
   src <- match.arg(src.comp)
-  site <- base::match(scr, afr_extract())
+  site <- base::grep(scr, afr_extract())
+  wb <- afr_extract()[site]
+
+  if(search){
+    st <- httr::GET(paste0(wb,project))$status_code
+  }else{
+    stop("stop",call.=F)
+  }
+
+ifelse(st!=404,"yes","no")
 
 }
