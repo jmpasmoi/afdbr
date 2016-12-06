92507107393b4a2acbea76a8860011dfe44f8697
Jean Marie Cimula <jmkacim@gmail.com>
2016-12-06 14:23:37 +0100

    some other functions

--- R/depot.R
+++ R/depot.R
@@ -37,15 +37,54 @@ apm <- dplyr::select(df, country, status, segment, project_id, amount)
 #Getting date of projects
 aprj <- dplyr::select(df, segment, country, status, segment, project_id, amount, appraisal_date, approval_date, start_date)
 
-pmpr <- dplyr::filter(aprj, as.Date(start_date,"%d/%m/%Y") >= as.Date("01/01/2014","%d/%m/%Y") & as.Date(start_date,"%d/%m/%Y") <= as.Date("21/05/2017","%d/%m/%Y"))
+##pmpr <- dplyr::filter(aprj, as.Date(start_date,"%d/%m/%Y") >= as.Date("01/01/2010","%d/%m/%Y") & as.Date(start_date,"%d/%m/%Y") <= as.Date("21/05/2017","%d/%m/%Y"))
+
+pmpr <- dplyr::filter(aprj, as.Date(start_date,"%d/%m/%Y") >= as.Date("01/01/2010","%d/%m/%Y"))
 
 
 sqldf::sqldf("
-select segment, country,
+select segment, country, substr(start_date,7,10) startdate,
 count(case when status = 'ongoing' then status end) as ongoing,
-count(case when status = 'approved' then status end) as approved 
-from pmpr group by segment,  country
+count(case when status = 'approved' then status end) as approved,
+count(case when status = 'pipeline' then status end) as pipeline,
+count(case when status = 'lending' then status end) as lending
+from pmpr group by segment, country, substr(start_date,7,10)
 ")
+
+
+cf <- sqldf::sqldf("
+select segment, country, substr(start_date,7,10) startdate,
+                   count(case when status = 'ongoing' then status end) as ongoing,
+                   count(case when status = 'approved' then status end) as approved,
+                   count(case when status = 'pipeline' then status end) as pipeline,
+                   count(case when status = 'lending' then status end) as lending
+                   from pmpr group by segment, country, substr(start_date,7,10)
+                   ")
+
+cfon <- cf[,c("segment","country","startdate","ongoing")]
+
+cfon <- sqldf::sqldf("
+select country, startdate, sum(ongoing) ongoing
+from cfon 
+where startdate >= 2010 and startdate <= 2016 and country not like '%national%'
+group by  country, startdate
+")
+
+cfon <- tidyr::spread(cfon,startdate,ongoing)
+
+cftmp <- cfon
+
+rownames(cfon) <- cftmp$country
+remove(cftmp)
+cfon <- cfon[,-1]
+
+begin <- last <- 1:nrow(cfon)
+
+cfon <- cbind(begin,cfon,last)
+
+slopegraph(head(cfon, n=20), main = 'Ongoing Project of AfDB')
+
+
 ## afr_dashboard_country <- function(data) {
 ##if(missing(data)){
 ##    objects <- ls(pos = 1)    
@@ -56,3 +95,5 @@ from pmpr group by segment,  country
 ##  }
 ##  findf
 ##}
+
+ 
