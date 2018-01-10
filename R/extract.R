573171bf78c089a549e8446b82c1813d8fb5c795
Jean Marie Cimula <jmkacim@gmail.com>
2018-01-10 12:25:54 +0900

    data extraction

--- R/extract.R
+++ R/extract.R
@@ -1,29 +1,17 @@
 .afr.env <- new.env()
-
 .read.afr <- function() {
-
   filename <- system.file("afdbr", "afdbr.txt", package="afdbr")
-
   if (!file.exists(filename)) stop("Hm, file", filename, "is missing.", call.=FALSE)
-
   data <- readLines(filename, encoding="UTF-8", warn = F)
-
   data <- data[! grepl("^##", data)]
 }
 
 #' Segmenting African Market From African Development Bank Group
-#'
 #' @details
-#'
 #' Extract textual information
-#'
 #' @export
 afr_extract <- function (){
-
   if (is.null(.afr.env$afr.data)) .afr.env$afr.data <- .read.afr()
-
   afr <- .afr.env$afr.data
-
   return(afr)
-
 }
